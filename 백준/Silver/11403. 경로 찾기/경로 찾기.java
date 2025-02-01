bde8aba2843bd2cf80164c0378c72db3bc2f6b3c
Kim Se Eun <rlatpdms1014@gmail.com>
2025-02-01 14:30:50 +0900

    [Silver I] Title: 경로 찾기, Time: 208 ms, Memory: 18952 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[][] dist = new int[n][n];
+        
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] == 1 && dist[k][j] == 1) {
+                        dist[i][j] = 1;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                bw.write(dist[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+        
+        bw.close();
+        br.close();
+    }
+}
